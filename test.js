0f592e6ef1edd9903c0fe982c7e523858a3ee9c6
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-04-10 20:21:29 +0200

    add bit components

---
+++ test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default ({ children }) => (
+  <div
+    style={{
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: 'green',
+    }}>
+    {children}
+    
+  </div>
+)
