0030f85dd1507089a7d7ee845bdbb19e8d3d5b65
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-04-10 19:00:40 +0200

    first commit

---
+++ theme.js
@@ -0,0 +1,17 @@
+export default {
+  fonts: {
+    body: 'Roboto, sans-serif',
+    monospace: '"Roboto Mono", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: 'black',
+    primary: 'blue',
+
+  },
+  styles: {
+    a: {
+      color: 'yellow'
+    }
+  }
+}
